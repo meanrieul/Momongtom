92ec1493f7db5710cc2d655b378495edf8792b35
meanrieul <wp1270@users.noreply.github.com>
2021-03-20 18:34:25 +0900

    add css

--- clock.js
+++ clock.js
@@ -5,15 +5,13 @@ function getTime() {
     const date = new Date();
     const minutes = date.getMinutes();
     const hours = date.getHours();
-    const seconds = date.getSeconds();
     clockTitle.innerText = 
-    `${hours < 10 ? `0${hours}`: hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+    `${hours < 10 ? `0${hours}`: hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
     
 }
 function init() {
     getTime();
     setInterval(getTime,1000);
-
 }
 
 init();
--- weather.js
+++ weather.js
@@ -1,7 +1,47 @@
-const weather = document.querySelector(".js-weather");
+const weatherDeg = document.querySelector(".js-weather-deg"),
+    weatherLoc = document.querySelector(".js-weather-loc"),
+    weatherIcons = document.querySelectorAll(".weather__icon");
 
+const thunderstorm = document.querySelector(".thunderstorm"),
+    drizzle = document.querySelector(".drizzle"),
+    rain = document.querySelector(".rain"),
+    snow = document.querySelector(".snow"),
+    atmosphere = document.querySelector(".atmosphere"),
+    clear = document.querySelector(".clear"),
+    clouds = document.querySelector(".clouds");
 const API_KEY = "ad8ecc061e1074a8280989b533bc593a";
 const COORDS = "coords";
+
+function getCondition(condition) {
+    if(condition >= 200 && condition < 300) {
+        weatherIcons.forEach(icon=>icon.classList.remove(SHOWING_CN));
+        thunderstorm.classList.add(SHOWING_CN);
+    }
+    else if(condition >=300 && condition <400) {
+        weatherIcons.forEach(icon=>icon.classList.remove(SHOWING_CN));
+        drizzle.classList.add(SHOWING_CN);
+    }
+    else if(condition >=500 && condition <600) {
+        weatherIcons.forEach(icon=>icon.classList.remove(SHOWING_CN));
+        rain.classList.add(SHOWING_CN);
+    }
+    else if(condition >=600 && condition <700) {
+        weatherIcons.forEach(icon=>icon.classList.remove(SHOWING_CN));
+        snow.classList.add(SHOWING_CN);
+    }
+    else if(condition>=700 && condition < 800){
+        weatherIcons.forEach(icon=>icon.classList.remove(SHOWING_CN));
+        atmosphere.classList.add(SHOWING_CN);
+    }
+    else if(condition == 800) {
+        weatherIcons.forEach(icon=>icon.classList.remove(SHOWING_CN));
+        clear.classList.add(SHOWING_CN);
+    }
+    else if(condition>800){
+        weatherIcons.forEach(icon=>icon.classList.remove(SHOWING_CN));
+        clouds.classList.add(SHOWING_CN);
+    }
+}
 
 function getWeather(lat, lng) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
@@ -10,7 +50,12 @@ function getWeather(lat, lng) {
     }).then(function(json) {
         const temperature = json.main.temp;
         const place = json.name;
-        weather.innerText = `${temperature} @ ${place}`;
+        console.log(json);
+        const condition = parseInt(json.weather[0].id);
+        getCondition(condition);
+
+        weatherDeg.innerText = `${temperature}°C`;
+        weatherLoc.innerText = `at ${place}`;
     });
 
 }
